e0a5e0d8be431f38d1b19b50c20f30e881073fb0
Manju <manjunath960650@gmail.com>
2025-12-03 16:29:32 +0530

    terraform practice

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -23,3 +23,20 @@ resource "vault_generic_secret" "example" {
 }
 EOT
 }
+
+# resource "vault_mount" "" {
+#   path        = "kvv2"
+#   type        = "kv"
+#   options     = { version = "2" }
+#   description = "KV Version 2 secret engine mount"
+# }
+resource "vault_generic_secret" "roboshop_secrets" {
+  path = "roboshop-dev-secrets/cart"
+
+  data_json = <<EOT
+{
+  "username": "ec2-user",
+  "password": "DevOps321"
+}
+EOT
+}
